7dfdbcf0a1e56d510a82c78302c53dbd0afd976e
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-11 12:24:41 -0300

    ordenação por livros disponivels

--- JavaScript métodos de array/alura_books/app/metodoForEach.js
+++ JavaScript métodos de array/alura_books/app/metodoForEach.js
@@ -6,9 +6,10 @@ function exibirLivros(listaLivros) {
     elementoParaInserirLivros.innerHTML = '';
     listaLivros.forEach(livro => {
 
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
             ${livro.titulo}
         </h2>
@@ -23,3 +24,14 @@ function exibirLivros(listaLivros) {
     });
 
 };
+
+
+function verificardiponibilidadeLivro(livro) {
+    // if(livro.quantidade > 0) {
+    //     return 'livro__imagens';
+    // }else{
+    //     return 'livro__imagens indisponivel';
+    // }
+    
+
+}
