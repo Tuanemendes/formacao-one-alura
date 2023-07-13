798602e812413b62bf5c1846f642688258fc6683
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-12 23:34:07 -0300

    usando async await

--- JavaScript consumindo e tratando dados de uma API/README.md
+++ JavaScript consumindo e tratando dados de uma API/README.md
@@ -2,6 +2,27 @@
 
 ## Aplicando a assincronicidade
 
+construir um código com o  .then, um método que as Promises disponibilizam:
+
+```
+var requisicao = fetch("https://localhost:5000/")
+.then(resposta => resposta.json())
+.then(respostaConvertida => console.log(respostaConvertida));
+
+```
+
+reproduzir o mesmo código usando async await:
+
+```
+async function geraRequisicao() {
+    var requisicao = await fetch(“https://localhost:5000”);
+    var respostaConvertida = await requisicao.json();
+}
+
+```
+
+
+
 Síncrono ou Assíncrono?
 Event Loop e Call Stack
 Leitura síncrona
--- JavaScript consumindo e tratando dados de uma API/js-consumindo-dados-api/script.js
+++ JavaScript consumindo e tratando dados de uma API/js-consumindo-dados-api/script.js
@@ -1,12 +1,21 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe!')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluido!'));
+// var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+//     .then(resposta => resposta.json())
+//     .then(r => {
+//         if (r.erro) {
+//             throw Error('Esse cep não existe!')
+//         } else
+//             console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('Processamento concluido!'));
 
-console.log(consultaCEP);
+// console.log(consultaCEP);
+
+async function buscarEndereco() {
+
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCEPConvertido = await consultaCEP.json(consultaCEP);
+    console.log(consultaCEPConvertido);
+}
+
+buscarEndereco();
